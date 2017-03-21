97d9817dc4e47543d3ed6d7a837b3f6c5bda7c48
RenCloud <thedarkrencloud@yandex.ru>
2017-03-21 10:28:41 +0100

    Comments

--- src/de/luh/pps/watershedplugin/TestSegmentationModule.java
+++ src/de/luh/pps/watershedplugin/TestSegmentationModule.java
@@ -29,17 +29,25 @@ import threads.SegmentingThread;
 import yplugins.YModule;
 
 public class TestSegmentationModule extends GMPanel implements YModule, YObserver {
+	
+	//The "Generate" Start button
 	private JButton Start;
+	//Textfield for min Value
 	private JFormattedTextField min;
+	//Textfield for maxvalue
 	private JFormattedTextField max;
+	//Slider for min max slection
 	private JRangeSlider slider;
+	//Histopanel
 	private HistoPanel hp;
 	//Selected High and Toplevel from User
 	private int maxLevel;
 	private int minLevel;
+	//Need actually 
+	//TODO: TRY TO LOAD DATA WITHOUT USER TASK ... 
 	private boolean first=true;
 	
-	
+	//OLD CLASS FROM GIVEN SAMPLE 
 	private class SampleSegThread extends SegmentingThread {
 		public SampleSegThread(Segment seg, boolean monitor) {
 			super(seg, monitor);
@@ -96,24 +104,30 @@ public class TestSegmentationModule extends GMPanel implements YModule, YObserve
 	
 	public TestSegmentationModule() {
 		
-		this.Start = new JButton("Do it!");
+		//Ini Generate Button 
+		this.Start = new JButton("Generate");
+		//Set Action Listener when the button was clicked
 		this.Start.addActionListener(new ActionListener() {
 			public void actionPerformed(final ActionEvent e) {
+				//Actually start the old sample				
 				Segment tmp_seg = MasterControl.get_is().get_segment(ToolSegGen.TMP_SEG_NAME);
 				SampleSegThread my_thread = new SampleSegThread(tmp_seg, true);
 				my_thread.start();
 			}
 		});
 		
+		//Create a Load button for data not nice but i search for a better solution
 		JButton load = new JButton("Load HP");
 		 load.addActionListener(new ActionListener() {
 			public void actionPerformed(final ActionEvent e) {
 				int i = 1;
 				if(first){
+					//don't know why but it need to run 3 time for the first run, so that the user does not push it 3 times we do it for him
 					i=3;
 					first = false;
 				}
 				for(int j = 0;j<i;j++){
+					//load stack to set data
 					ImageStack is = MasterControl.get_is();
 					if(is.get_state()==2){
 						minLevel =is.get_vch().get_nonzero_min();
@@ -129,52 +143,48 @@ public class TestSegmentationModule extends GMPanel implements YModule, YObserve
 				
 				
 			}
-		});
-
-		add("do_it", this.Start);
-		
-		/*set_layout(""+
-			"<table>"+
-			  "<tr>"+
-			    "<td>A simple Button:</td>"+
-			    "<td>::do_it::</td>"+
-			  "</tr>"+
-			"</table>");*/				
-
-		ImageStack is = MasterControl.get_is();
-		
+		});	
+	 				
+		 
+		ImageStack is = MasterControl.get_is();		
 		is.addObserver(this, "Test Module Listener");
+		//Create min Textfield and add a change listener for input 
 		this.min = new JFormattedTextField(NumberFormat.getIntegerInstance());
 		this.min.setColumns(8);
 		this.min.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
+				//get new Value, set Slider and histopanel highlight new 
 				int minTemp = Integer.parseInt(min.getText());
 				minLevel=minTemp;
 				slider.setRange(minLevel,maxLevel);
 				hp.highlight_interval(minLevel, maxLevel, new Color(255, 0, 0, 80));
 			}
 		});
-		
+		//Create max Textfield and add a change listener for input 
 		this.max = new JFormattedTextField(NumberFormat.getIntegerInstance());
 		this.max.setColumns(8);
 		this.max.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
+				//get new Value, set Slider and histopanel highlight new 
 				int maxTemp = Integer.parseInt(max.getText());
 				maxLevel=maxTemp;
 				slider.setRange(minLevel,maxLevel);
 				hp.highlight_interval(minLevel, maxLevel, new Color(255, 0, 0, 80));
 			}
 		});
-	
+		
+		//Create the histopanel
 		this.hp=new HistoPanel(is.get_vch(),"HPTest",false,false);
 		JPanel hpPanel = new JPanel();
 		hpPanel.setBorder(new EmptyBorder(0,16,0,16));;
 		hpPanel.setLayout(new BorderLayout());
 		hpPanel.add(this.hp, "Center");
+		//Create slider and add change listener 
 		this.slider = new JRangeSlider(0,1000,100,300,1);
 		this.slider.addChangeListener(new ChangeListener(){
 			 public void stateChanged(ChangeEvent e)
 		      {
+				//get new Value, histopanel highlight and textfields new 
 		       minLevel=slider.getLowValue();
 		        maxLevel=slider.getHighValue();
 		        
@@ -183,12 +193,14 @@ public class TestSegmentationModule extends GMPanel implements YModule, YObserve
 				hp.highlight_interval(minLevel, maxLevel, new Color(255, 0, 0, 80));
 		      }
 		});
-		
+		//Add components
+		add("generate", this.Start);	
 		add("min",this.min);
 		add("max",this.max);
 		add("hp",hpPanel);
 		add("slider",this.slider);
 		add("load",load);
+		//Set layout
 		set_layout(
 			      "<table width='100%' height='100%' cellpadding='0' border='0'>  <tr height='97%'> "
 			      + "   <td fill='both'>::hp::</td>  </tr><tr height='6%'> "
@@ -204,7 +216,7 @@ public class TestSegmentationModule extends GMPanel implements YModule, YObserve
 			      + "  <td fill='horizontal'>      <table width='100%' cellpadding='0' cellspacing='0' margin='0' border='0'>  "
 			      + "      <tr>            "
 			      + "        <td width='98%' >            <table cellpadding='0' cellspacing='0' margin='0'>   "
-			      + "           <tr>                <td>::do_it::</td>  <td>::load::</td>                "
+			      + "           <tr>                <td>::generate::</td>  <td>::load::</td>                "
 			      + "             </tr>            </table>          </td> "
 			      + "        "
 			      + "        </tr>      </table>    </td>  </tr></table>");
@@ -212,7 +224,7 @@ public class TestSegmentationModule extends GMPanel implements YModule, YObserve
 		
 		
 		
-		//SegGenRangeGui
+	 
 		
 	}
 	
